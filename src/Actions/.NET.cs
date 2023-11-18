e0d0bfd030a63da2303e608f1379e59f1fb4400b
he3als <he3als@ameliorated.io>
2023-11-18 16:31:35 +0000

    feat: .NET 3.5 with USB/folder

--- src/Actions/.NET.cs
+++ src/Actions/.NET.cs
@@ -8,6 +8,7 @@ using System.Security;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using Misc.FolderPicker;
 using Ameliorated.ConsoleUtils;
 using Microsoft.Dism;
 
@@ -15,181 +16,195 @@ namespace amecs.Actions
 {
     public class _NET
     {
+        private static string file;
+        private static bool CheckFileViolation(string inputFile)
+        {
+            try
+            {
+                file = inputFile;
+            }
+            catch (SecurityException e)
+            {
+                Console.WriteLine();
+                ConsoleTUI.OpenFrame.Close("Security error: " + e.Message, ConsoleColor.Red, Console.BackgroundColor, new ChoicePrompt() { AnyKey = true, Text = "Press any key to return to the Menu: " });
+                return true;
+            }
+
+            return false;
+        }
+        
+        private static bool DismountImage()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "PowerShell.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = $"-NoP -C \"Dismount-DiskImage '{file}'\"";
+            startInfo.RedirectStandardOutput = true;
+
+            var proc = Process.Start(startInfo);
+            proc.WaitForExit();
+            return true;
+        }
+        
         public static bool Install()
         {
-            var key = new ChoicePrompt() { Text = "A Windows ISO must be provided to install .NET 3.5.\r\nPress any key to select an ISO: ", AnyKey = true }.Start();
-
-            if (key == null)
-                return false;
+            string letter;
             
-            var dialog = new System.Windows.Forms.OpenFileDialog();
-            dialog.Filter = "ISO Files (*.ISO)| *.ISO"; // Filter files by extension
-            dialog.Multiselect = false;
-            dialog.InitialDirectory = Globals.UserFolder;
-
-            NativeWindow window = new NativeWindow();
-            window.AssignHandle(Process.GetCurrentProcess().MainWindowHandle);
-            if (dialog.ShowDialog(window) == DialogResult.OK)
+            var choice = new ChoicePrompt() { Text = "A Windows Setup image must be provided to install .NET 3.5.\r\nWould you like to use a Windows Setup USB/folder instead of an ISO? (Y/N): " }.Start();
+            if (choice == null) return false;
+            var usingFolder = choice == 0;
+            if (usingFolder)
             {
-                string file;
-                try
+                var dlg = new FolderPicker();
+                if (dlg.ShowDialog(default, false) == false) return false;
+                if (CheckFileViolation(dlg.ResultPath)) return false;
+                letter = dlg.ResultPath;
+            }
+            else
+            {
+                var dialog = new System.Windows.Forms.OpenFileDialog();
+                dialog.Filter = "ISO Files (*.ISO)| *.ISO"; // Filter files by extension
+                dialog.Multiselect = false;
+                dialog.InitialDirectory = Globals.UserFolder;
+
+                NativeWindow window = new NativeWindow();
+                window.AssignHandle(Process.GetCurrentProcess().MainWindowHandle);
+                if (dialog.ShowDialog(window) == DialogResult.OK)
                 {
-                    file = dialog.FileName;
-                } catch (SecurityException e)
-                {
+                    if (CheckFileViolation(dialog.FileName)) return false;
+                    
                     Console.WriteLine();
-                    ConsoleTUI.OpenFrame.Close("Security error: " + e.Message, ConsoleColor.Red, Console.BackgroundColor, new ChoicePrompt() { AnyKey = true, Text = "Press any key to return to the Menu: " });
-                    return false;
-                }
-                
-                Console.WriteLine();
-                ConsoleTUI.OpenFrame.WriteCentered("\r\nMounting ISO");
-
-                string letter;
-                using (new ConsoleUtils.LoadingIndicator(true))
-                {
-                    ProcessStartInfo startInfo = new ProcessStartInfo();
-                    startInfo.CreateNoWindow = false;
-                    startInfo.UseShellExecute = false;
-                    startInfo.FileName = "PowerShell.exe";
-                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                    startInfo.Arguments = $"-NoP -C \"(Mount-DiskImage '{file}' -PassThru | Get-Volume).DriveLetter\"";
-                    startInfo.RedirectStandardOutput = true;
-
-                    var proc = Process.Start(startInfo);
-                    proc.WaitForExit();
-
-                    letter = proc.StandardOutput.ReadLine();
-                }
-
-
-
-                
-                if (!Directory.Exists(letter + @":\sources\sxs") || !Directory.GetFiles(letter + @":\sources\sxs", "*netfx3*").Any())
-                {
-                    try
+                    ConsoleTUI.OpenFrame.WriteCentered("\r\nMounting ISO");
+                    
+                    using (new ConsoleUtils.LoadingIndicator(true))
                     {
                         ProcessStartInfo startInfo = new ProcessStartInfo();
                         startInfo.CreateNoWindow = false;
                         startInfo.UseShellExecute = false;
                         startInfo.FileName = "PowerShell.exe";
                         startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                        startInfo.Arguments = $"-NoP -C \"Dismount-DiskImage '{file}'\"";
+                        startInfo.Arguments = $"-NoP -C \"(Mount-DiskImage '{file}' -PassThru | Get-Volume).DriveLetter + ':'\"";
                         startInfo.RedirectStandardOutput = true;
 
                         var proc = Process.Start(startInfo);
                         proc.WaitForExit();
-                    } catch (Exception e)
-                    {
+
+                        letter = proc.StandardOutput.ReadLine();
                     }
-
-                    Console.WriteLine();
-                    ConsoleTUI.OpenFrame.Close("ISO does not contain the required files.", ConsoleColor.Red, Console.BackgroundColor, new ChoicePrompt() { AnyKey = true, Text = "Press any key to return to the Menu: " });
-                    return false;
-                }
-
-                ConsoleTUI.OpenFrame.WriteCentered("\r\nInstalling .NET 3.5");
-                var topCache = Console.CursorTop;
-                var leftCache = Console.CursorLeft;
-                Console.WriteLine();
-                bool inProgress = false;
-                try
-                {
-                    using (var indicator = new ConsoleUtils.LoadingIndicator())
+                    
+                    if (!Directory.Exists(letter + @"\sources\sxs") || !Directory.GetFiles(letter + @"\sources\sxs", "*netfx3*").Any())
                     {
-                        DismApi.Initialize(DismLogLevel.LogErrors);
-                        using (var session = DismApi.OpenOnlineSession())
+                        try
                         {
-                            var stdout = GetStdHandle(-11);
-                            bool indicatorStopped = false;
-                            var maxHashTags = (ConsoleTUI.OpenFrame.DisplayWidth - 5);
-                            DismApi.EnableFeatureByPackagePath(session, "NetFX3", null, true, true, new List<string>() { letter + @":\sources\sxs" }, delegate(DismProgress progress)
-                            {
-                                inProgress = true;
-                                if (!indicatorStopped)
-                                {
-                                    indicator.Stop();
-                                    Console.SetCursorPosition(leftCache, topCache);
-                                    Console.WriteLine("   ");
-                                }
+                            ProcessStartInfo startInfo = new ProcessStartInfo();
+                            startInfo.CreateNoWindow = false;
+                            startInfo.UseShellExecute = false;
+                            startInfo.FileName = "PowerShell.exe";
+                            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            startInfo.Arguments = $"-NoP -C \"Dismount-DiskImage '{file}'\"";
+                            startInfo.RedirectStandardOutput = true;
 
-                                indicatorStopped = true;
-                                var progressPerc = progress.Current / 10;
-                                var currentHashTags = (int)Math.Ceiling(Math.Min(((double)progressPerc / 100) * maxHashTags, maxHashTags));
-                                var spaces = maxHashTags - currentHashTags + (4 - progressPerc.ToString().Length);
-                                var sb = new StringBuilder(new string('#', currentHashTags) + new string(' ', spaces) + progressPerc + "%");
-                                uint throwaway;
-                                WriteConsoleOutputCharacter(stdout, sb, (uint)sb.Length, new Languages.COORD((short)ConsoleTUI.OpenFrame.DisplayOffset, (short)Console.CursorTop), out throwaway);
-                                inProgress = false;
-                            });
-                            session.Close();
-                            Thread.Sleep(100);
-                            var sb = new StringBuilder(new string('#', maxHashTags) + " 100%");
-                            uint throwaway;
-                            WriteConsoleOutputCharacter(stdout, sb, (uint)sb.Length, new Languages.COORD((short)ConsoleTUI.OpenFrame.DisplayOffset, (short)Console.CursorTop), out throwaway);
+                            var proc = Process.Start(startInfo);
+                            proc.WaitForExit();
+                        } catch (Exception e)
+                        {
                         }
 
-                        DismApi.Shutdown();
-
-                        ProcessStartInfo startInfo = new ProcessStartInfo();
-                        startInfo.CreateNoWindow = false;
-                        startInfo.UseShellExecute = false;
-                        startInfo.FileName = "PowerShell.exe";
-                        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                        startInfo.Arguments = $"-NoP -C \"Dismount-DiskImage '{file}'\"";
-                        startInfo.RedirectStandardOutput = true;
-
-                        var proc = Process.Start(startInfo);
-                        proc.WaitForExit();
+                        Console.WriteLine();
+                        ConsoleTUI.OpenFrame.Close("ISO does not contain the required files.", ConsoleColor.Red, Console.BackgroundColor, new ChoicePrompt() { AnyKey = true, Text = "Press any key to return to the Menu: " });
+                        return false;
                     }
-                } catch (Exception e)
+                }
+                else
                 {
-                    while (inProgress)
+                    Console.WriteLine();
+                    ConsoleTUI.OpenFrame.Close("\r\nYou must select an ISO.", new ChoicePrompt() {AnyKey = true, Text = "Press any key to return to the Menu: "});
+                    return true;
+                }
+            }
+
+            ConsoleTUI.OpenFrame.WriteCentered("\r\nInstalling .NET 3.5");
+            var topCache = Console.CursorTop;
+            var leftCache = Console.CursorLeft;
+            Console.WriteLine();
+            bool inProgress = false;
+            try
+            {
+                using (var indicator = new ConsoleUtils.LoadingIndicator())
+                {
+                    DismApi.Initialize(DismLogLevel.LogErrors);
+                    using (var session = DismApi.OpenOnlineSession())
                     {
-                        Thread.Sleep(50);
+                        var stdout = GetStdHandle(-11);
+                        bool indicatorStopped = false;
+                        var maxHashTags = (ConsoleTUI.OpenFrame.DisplayWidth - 5);
+                        DismApi.EnableFeatureByPackagePath(session, "NetFX3", null, true, true, new List<string>() { letter + @"\sources\sxs" }, delegate(DismProgress progress)
+                        {
+                            inProgress = true;
+                            if (!indicatorStopped)
+                            {
+                                indicator.Stop();
+                                Console.SetCursorPosition(leftCache, topCache);
+                                Console.WriteLine("   ");
+                            }
+
+                            indicatorStopped = true;
+                            var progressPerc = progress.Current / 10;
+                            var currentHashTags = (int)Math.Ceiling(Math.Min(((double)progressPerc / 100) * maxHashTags, maxHashTags));
+                            var spaces = maxHashTags - currentHashTags + (4 - progressPerc.ToString().Length);
+                            var sb = new StringBuilder(new string('#', currentHashTags) + new string(' ', spaces) + progressPerc + "%");
+                            uint throwaway;
+                            WriteConsoleOutputCharacter(stdout, sb, (uint)sb.Length, new Languages.COORD((short)ConsoleTUI.OpenFrame.DisplayOffset, (short)Console.CursorTop), out throwaway);
+                            inProgress = false;
+                        });
+                        session.Close();
+                        Thread.Sleep(100);
+                        var sb = new StringBuilder(new string('#', maxHashTags) + " 100%");
+                        uint throwaway;
+                        WriteConsoleOutputCharacter(stdout, sb, (uint)sb.Length, new Languages.COORD((short)ConsoleTUI.OpenFrame.DisplayOffset, (short)Console.CursorTop), out throwaway);
                     }
 
+                    DismApi.Shutdown();
+                    if (usingFolder) DismountImage();
+                }
+            } catch (Exception e)
+            {
+                while (inProgress)
+                {
+                    Thread.Sleep(50);
+                }
+
+                if (usingFolder)
+                {
                     try
                     {
-                        ProcessStartInfo startInfo = new ProcessStartInfo();
-                        startInfo.CreateNoWindow = false;
-                        startInfo.UseShellExecute = false;
-                        startInfo.FileName = "PowerShell.exe";
-                        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                        startInfo.Arguments = $"-NoP -C \"Dismount-DiskImage '{file}'\"";
-                        startInfo.RedirectStandardOutput = true;
-
-                        var proc = Process.Start(startInfo);
-                        proc.WaitForExit();
-                    } catch (Exception ex)
+                        DismountImage();
+                    }
+                    catch (Exception ex)
                     {
                     }
-
-                    Console.WriteLine();
-                    Console.WriteLine();
-                    ConsoleTUI.OpenFrame.Close("DISM error: " + e.Message, ConsoleColor.Red, Console.BackgroundColor, new ChoicePrompt()
-                    {
-                        AnyKey = true,
-                        Text = "Press any key to return to the Menu: "
-                    });
-                    return false;
                 }
-                
+
                 Console.WriteLine();
                 Console.WriteLine();
-                ConsoleTUI.OpenFrame.Close(".NET 3.5 installed successfully", ConsoleColor.Green, Console.BackgroundColor, new ChoicePrompt()
+                ConsoleTUI.OpenFrame.Close("DISM error: " + e.Message, ConsoleColor.Red, Console.BackgroundColor, new ChoicePrompt()
                 {
                     AnyKey = true,
                     Text = "Press any key to return to the Menu: "
                 });
-                return true;
+                return false;
             }
-            else
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            ConsoleTUI.OpenFrame.Close(".NET 3.5 installed successfully", ConsoleColor.Green, Console.BackgroundColor, new ChoicePrompt()
             {
-                Console.WriteLine();
-                ConsoleTUI.OpenFrame.Close("\r\nYou must select an ISO.", new ChoicePrompt() {AnyKey = true, Text = "Press any key to return to the Menu: "});
-                return true;
-            }
+                AnyKey = true,
+                Text = "Press any key to return to the Menu: "
+            });
+            return true;
         }
 
         [StructLayout(LayoutKind.Sequential)]
